eade167f806c8960ee9bae3e71f5e598207f9afc
LaTaevia Berry <lataevia.berry@gmail.com>
2016-03-25 14:33:22 -0700

    add varibles assigned to each quiz question, and full width beach image

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,47 @@ $(document).ready(function() {
   $("form").submit(function(event){
     var coffeeInput = $("input:radio[name=coffee]:checked").val();
     var zombieInput = $("input:radio[name=zombie]:checked").val();
-    if (coffeeInput==="cream" && zombieInput==="water"){
+    var annoyInput = $("input:radio[name=annoy]:checked").val();
+    var lrcInput = $("input:radio[name=lrc]:checked").val();
+    var collabInput = $("input:radio[name=collab]:checked").val();
+    if (coffeeInput==="cream"){
       $(".machu-hide").show();
-    } else{
-      alert(zombieInput);
+    };
+    if (coffeeInput==="blackcoffee"){
+      $(".cold-hide").show();
+    };
+    if (coffeeInput==="sugar"){
+      $(".warm-hide").show();
     };
     event.preventDefault();
   });
 });
+
+    // } else if (coffeeInput==="cream" && zombieInput==="water" && annoyInput==="hipsters" && lrcInput==="ambi" && collabInput==="elton"){
+    //   $(".warm-hide").show();
+
+
+    // if (coffeeInput==="cream" && zombieInput==="water"){
+    //   $(".machu-hide").show();
+    // };
+    // if (coffeeInput==="cream" && zombieInput==="water"){
+    //   $(".machu-hide").show();
+    // };
+    // if (coffeeInput==="cream" && zombieInput==="water"){
+    //   $(".machu-hide").show();
+    // };
+    // if (coffeeInput==="cream" && zombieInput==="water"){
+    //   $(".machu-hide").show();
+    // };
+    // if (coffeeInput==="cream" && zombieInput==="water"){
+    //   $(".machu-hide").show();
+    // };
+    // if (coffeeInput==="cream" && zombieInput==="water"){
+    //   $(".machu-hide").show();
+    // };
+    // if (coffeeInput==="cream" && zombieInput==="water"){
+    //   $(".machu-hide").show();
+    // };
 
 // if (coffeeInput==="Cream"){
 //   alert(coffeeInput);
