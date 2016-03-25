1fc0c04234b4e85ff239a7c297bb8d4d8f0ba521
LaTaevia Berry <lataevia.berry@gmail.com>
2016-03-25 10:45:57 -0700

    add finctioning HTML an JS sample to follow for project to function properly

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("form").submit(function(event){
+    var coffeeInput = $("input:radio[name=coffee]:checked").val();
+    var zombieInput = $("input:radio[name=zombie]:checked").val();
+    if (coffeeInput==="cream"){
+      alert(coffeeInput);
+    } else{
+      alert(zombieInput);
+    };
+    event.preventDefault();
+  });
+});
+
+// if (coffeeInput==="Cream"){
+//   alert(coffeeInput);
+// } else{
+//   alert(zombieInput);
+// };
+// $this = $(this);
+// var value = $this.find('input:radio[name=zombie]:checked').val();
+// alert(value);
